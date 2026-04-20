60128d85c839f0809908802fd9fb7b029540061f
lucas-amaral <lucas.a2.a2@gmail.com>
2026-04-20 06:47:51 -0300

    Set railway jdk version to 17

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,16 +13,6 @@ plugins {
 group = "com.amaral.home"
 version = "0.0.1-SNAPSHOT"
 
-java {
-    toolchain {
-        languageVersion = JavaLanguageVersion.of(21)
-    }
-}
-
-kotlin {
-    jvmToolchain(21)
-}
-
 repositories {
     mavenCentral()
 }
@@ -42,12 +32,12 @@ dependencies {
 tasks.withType<KotlinJvmCompile> {
     compilerOptions {
         freeCompilerArgs.add("-Xjsr305=strict")
-        jvmTarget.set(JvmTarget.fromTarget("21"))
+        jvmTarget.set(JvmTarget.fromTarget("17"))
     }
 }
 
 tasks.withType<JavaCompile> {
-    options.release.set(21)
+    options.release.set(17)
 }
 
 tasks.withType<Test> {
